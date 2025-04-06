50221b0a4a4294ad91778d2c5e7d225fee8d9e3c
Shivansh Tiwari <sachintiwari.edu@gmail.com>
2025-04-06 16:45:23 +0530

    Implement different pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'package:meal_app/categories.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_app/screens/categories.dart';
+
+
 
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
       brightness: Brightness.dark,
       seedColor: const Color.fromARGB(255, 131, 57, 0)),
-  textTheme: GoogleFonts.latoTextTheme(),
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
 );
 
 void main() {
@@ -21,7 +27,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const  CategoriesScreen(),
-    );
+      home: const CategoriesScreen()
+      );
+
   }
 }
