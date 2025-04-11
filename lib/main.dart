6850e5f5f52647c591c87941a38a3258e6a63cdf
Shivansh Tiwari <sachintiwari.edu@gmail.com>
2025-04-11 20:29:49 +0530

    Implements favorites_provider and meals_provider file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/screens/tabs.dart';
 //import 'package:meal_app/screens/categories.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 
@@ -18,7 +19,10 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(
+    child: App(),
+  ),
+    );
 }
 
 class App extends StatelessWidget {
---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_app/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final mealsFavourite = state.contains(meal);
+
+    if (mealsFavourite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
