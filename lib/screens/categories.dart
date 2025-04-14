e42db03a214a3f86d7ae4ee764253f3f3e9caf5b
Shivansh Tiwari <sachintiwari.edu@gmail.com>
2025-04-14 23:06:04 +0530

    Add animation implicit and explicit

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,15 +5,42 @@ import 'package:meal_app/widgets/category_grid_item.dart';
 import 'package:meal_app/models/category.dart';
 import 'package:meal_app/models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, 
   required this.availableMeals});
 
  
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+    
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -31,7 +58,9 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return AnimatedBuilder(
+      animation: _animationController, 
+      child: GridView(
       padding: const EdgeInsets.all(24),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
@@ -40,12 +69,7 @@ class CategoriesScreen extends StatelessWidget {
         mainAxisSpacing: 20,
       ),
       children: [
-        // ...availableCategories.map((category) => CatergoryGridItem(
-        //   category: category,
-        //   onSelectCategory: () {
-        //     _selectCategory(context, category);
-        //   },
-        // )),
+        
         for (final category in availableCategories)
           CatergoryGridItem(
               category: category,
@@ -53,6 +77,18 @@ class CategoriesScreen extends StatelessWidget {
                 _selectCategory(context, category);
               }),
       ],
-    );
+    ),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+            begin: const Offset(0, 0.3),
+            end: const Offset(0, 0),  
+          ).animate(CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        ),
+          child: child,
+          ),
+    ); 
   }
 }
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,9 +13,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-     final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
 
-     final isFavorite =  favoriteMeals.contains(meal);
+    final isFavorite = favoriteMeals.contains(meal);
 
     return Scaffold(
       appBar: AppBar(
@@ -24,29 +24,43 @@ class MealDetailsScreen extends ConsumerWidget {
           IconButton(
             onPressed: () {
               final wasAdded = ref
-                .read(favoriteMealsProvider.notifier)
-                .toggleMealFavoriteStatus(meal);
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
               ScaffoldMessenger.of(context).clearSnackBars();
               ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(wasAdded
-          ? 'Meal added to favorites!'
-          : 'Meal removed.'),
-      ),
-    );
+                SnackBar(
+                  content: Text(
+                      wasAdded ? 'Meal added to favorites!' : 'Meal removed.'),
+                ),
+              );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 14),
             Text(
